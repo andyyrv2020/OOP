247eff61828a340530b7813cc06f372ec7ba53a3
pc1_24 <pc1_24@DESKTOP-B4HELHV>
2024-01-09 15:55:55 +0200

    Added Interface

---
+++ CarsInterface/ICar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsInterface
+{
+    internal class ICar
+    {
+        public string Model { get; }
+        public string Color { get; }
+        public void Start();
+        public void Stop(); 
+
+
+    }
+}
