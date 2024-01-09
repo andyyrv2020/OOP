88519853d554469167a3965e4e541dfc143df631
pc1_24 <pc1_24@DESKTOP-B4HELHV>
2024-01-09 16:47:44 +0200

    Made Classes and Interfaces but they are empty

---
+++ AnimalKingdom/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimalKingdom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
