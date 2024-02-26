615e942529f571bb2d3f50b1aae8b6535a804ac9
PC-23 <it.pgee.burgas@gmail.com>
2024-02-26 12:09:18 +0200

    duda

--- ThreadsExample/Program.cs
+++ ThreadsExample/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Thread th1 = new Thread(CountBackwards);  
+            Thread th2 = new Thread(CountBackwards);
+            th1.Start();
+            th2.Start();
         }
+        static Random random = new Random();
+        static void CountBackwards()
+        {
+            int waitTime = random.Next(1, 10) * 1000;
+            Console.WriteLine($"Wait time: {waitTime}");
+
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(waitTime);    
+            }
+        }
+
     }
 }
---
+++ ThreadsRacing/Program.cs
@@ -0,0 +1,40 @@
+﻿    namespace ThreadsRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread player1 = new Thread(Player1);
+            Thread player2 = new Thread(Player2);
+            player1.Start();
+            player2.Start();
+        }
+        static Random random = new Random();
+        static void Player1()
+        {
+            int currentPoints = 0;
+            int endPoints = 100;
+
+            while (currentPoints <= endPoints)
+            {
+                currentPoints += random.Next(1, 10);
+                Console.WriteLine($"Player 1 Points: {currentPoints}");
+                System.Threading.Thread.Sleep( 1000 );
+            }
+            Console.WriteLine("Player 1 Finished");
+        }
+        static void Player2()
+        {
+            int currentPoints = 0;
+            int endPoints = 100;
+
+            while (currentPoints <= endPoints)
+            {
+                currentPoints += random.Next(1, 10);
+                Console.WriteLine($"Player 2 Points: {currentPoints}");
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine("Player 2 Finished");
+        }
+    }
+}
