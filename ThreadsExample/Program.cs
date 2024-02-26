f0d431dd57cc2974ead674903982c865e91c78ed
PC-23 <it.pgee.burgas@gmail.com>
2024-02-26 11:48:57 +0200

    created

---
+++ ThreadsExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThreadsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
