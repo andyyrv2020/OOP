26bfa06268e1185d46b6855a5b5379bcab8f118c
PC-23 <it.pgee.burgas@gmail.com>
2024-03-19 08:04:30 +0200

    hw

---
+++ Library/Library.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Library : IEnumerable
+    {
+        private List<Book> books;
+
+        public Library(params Book[] initialBooks)
+        {
+            books = new List<Book>(initialBooks);
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
