65c5df42e46fea898392d2e7cb90482370b6f217
PC-23 <it.pgee.burgas@gmail.com>
2024-04-15 09:01:02 +0300

    komit

---
+++ GasStation/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace GasStation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fillUp_1 = new System.Windows.Forms.Button();
+            this.fillUp_2 = new System.Windows.Forms.Button();
+            this.fillUp_3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fillUp_1
+            // 
+            this.fillUp_1.Location = new System.Drawing.Point(12, 345);
+            this.fillUp_1.Name = "fillUp_1";
+            this.fillUp_1.Size = new System.Drawing.Size(212, 23);
+            this.fillUp_1.TabIndex = 0;
+            this.fillUp_1.Text = "Зареди";
+            this.fillUp_1.UseVisualStyleBackColor = true;
+            // 
+            // fillUp_2
+            // 
+            this.fillUp_2.Location = new System.Drawing.Point(352, 345);
+            this.fillUp_2.Name = "fillUp_2";
+            this.fillUp_2.Size = new System.Drawing.Size(212, 23);
+            this.fillUp_2.TabIndex = 1;
+            this.fillUp_2.Text = "Зареди";
+            this.fillUp_2.UseVisualStyleBackColor = true;
+            // 
+            // fillUp_3
+            // 
+            this.fillUp_3.Location = new System.Drawing.Point(697, 345);
+            this.fillUp_3.Name = "fillUp_3";
+            this.fillUp_3.Size = new System.Drawing.Size(212, 23);
+            this.fillUp_3.TabIndex = 2;
+            this.fillUp_3.Text = "Зареди";
+            this.fillUp_3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::GasStation.Properties.Resources.gas_station;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(352, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 279);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::GasStation.Properties.Resources.gas_station;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 60);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(212, 279);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::GasStation.Properties.Resources.gas_station;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(697, 60);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(212, 279);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(694, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.fillUp_3);
+            this.Controls.Add(this.fillUp_2);
+            this.Controls.Add(this.fillUp_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fillUp_1;
+        private System.Windows.Forms.Button fillUp_2;
+        private System.Windows.Forms.Button fillUp_3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GasStation/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GasStation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Image RecolorImage(PictureBox pictureBox, double percentAnimation)
+        {
+            Bitmap original = new Bitmap(pictureBox.Image, pictureBox.Width, pictureBox.Height);
+            using (Graphics g = Graphics.FromImage(original))
+            {
+                ImageAttributes imageAttributes = CreateColorTable();
+                Rectangle coloredRectangle = CreateColoredRectangle(pictureBox, percentAnimation);
+                Bitmap croppedImage = this.Crop(original, coloredRectangle);
+
+                g.DrawImage(
+                    croppedImage,
+                    coloredRectangle,
+                    0, 0,
+                    coloredRectangle.Width, coloredRectangle.Height,
+                    GraphicsUnit.Pixel,
+                    imageAttributes
+
+                    );
+                return original;
+            }
+        }
+        private static ImageAttributes CreateColorTable()
+        {
+            ImageAttributes imageAttributes = new ImageAttributes();
+            ColorMap colorMap = new ColorMap();
+            colorMap.OldColor = Color.FromArgb(51, 51, 51);
+            colorMap.NewColor = Color.FromArgb(0, 255, 0);
+            ColorMap[] remapTable = {  colorMap };
+
+            imageAttributes.SetRemapTable(remapTable, ColorAdjustType.Bitmap);
+            return imageAttributes;
+        }
+
+        private static Rectangle CreateColoredRectangle(PictureBox pictureBox, double percentAnimation)
+        {
+            int heightAnimation = (int)(percentAnimation * pictureBox.Height);
+            Rectangle coloredRectangle = new Rectangle(
+                new Point(0, pictureBox.Height - heightAnimation),
+                new Size(pictureBox.Width, heightAnimation)
+                );
+            return coloredRectangle; 
+        }
+    }
+
+    private Bitmap Crop(Bitmap original, Rectangle coloredRectangle)
+    {
+        Bitmap cropped = new Bitmap(coloredRectangle.Width, coloredRectangle.Height);
+        using (Graphics g = Graphics.FromImage(cropped))
+        {
+            g.DrawImage(original, -coloredRectangle.X, -coloredRectangle.Y);
+            return cropped;
+        }
+    }
+}
