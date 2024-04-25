bb78fb58cce556ee000a061b9173bf43158e9f7b
PC04-204 <PC04-204@DESKTOP-Q8235VD>
2024-04-25 08:49:12 +0300

    Задача 3

---
+++ ObjectCompare/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableObject
+{
+    internal class Person : IComparable<Person>
+    {
+        public string Name { get;  set; }
+        public int Age { get;  set; }
+        public string City { get; set; }
+        public int CompareTo(Person other)
+        {
+            int nameComparison = Name.CompareTo(other.Name);
+            if (nameComparison != 0)
+                return nameComparison;
+
+            int ageComparison = Age.CompareTo(other.Age);
+            if (ageComparison != 0)
+                return ageComparison;
+
+            return City.CompareTo(other.City);
+        }
+    }
+}
---
+++ ObjectCompare/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ComparableObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                List<Person> people = new List<Person>();
+
+                string input;
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] tokens = input.Split();
+                    string name = tokens[0];
+                    int age = int.Parse(tokens[1]);
+                    string city = tokens[2];
+                    people.Add(new Person { Name = name, Age = age, City = city });
+                }
+
+                int N = int.Parse(Console.ReadLine());
+
+                Person selectedPerson = people[N - 1];
+                int sameCount = 0;
+                int differentCount = 0;
+
+                foreach (var person in people)
+                {
+                    int comparison = selectedPerson.CompareTo(person);
+                    if (comparison == 0)
+                        sameCount++;
+                    else
+                        differentCount++;
+                }
+
+                if (sameCount == 0)
+                {
+                    Console.WriteLine("No matches");
+                }
+                else
+                {
+                    Console.WriteLine($"{sameCount} {differentCount} {people.Count}");
+                }
+            }
+        }
+    }
+}
